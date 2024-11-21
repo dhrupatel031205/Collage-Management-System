3e9f2db7c0fcb51639cb59922c5ba17762710ae4
dhruvpatel031205 <d62077299@gmail.com>
2024-11-21 19:24:51 +0530

    yeap

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -24,18 +24,8 @@ export default function Navbar() {
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav">
                 <li className="nav-item">
-                  <Link className="nav-link active" aria-current="page" to="/">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/login">
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
                   <Link className="nav-link" to="/Signup">
-                    Signup
+                    Signout
                   </Link>
                 </li>
               </ul>
